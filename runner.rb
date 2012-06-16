ea2328e82330f970d5bfbe3f5ba6bad5d5761180
paranoiase Kang <paranoiase@gmail.com>
2012-06-16 10:45:38 +0900

    remove comment

--- runner.rb
+++ runner.rb
@@ -5,7 +5,6 @@ require './lib/crawler'
 get '/venture' do
   board = Board.new
   crawler = Crawler.new
-  # crawler.crawl(board, 'spec/fixture/list.html')
   crawler.crawl(board, 'http://www.bizinfo.go.kr/userPolicyInfoList.do?policyInfoSerchTO.lclasId=5000')
   board.to_rss
 end

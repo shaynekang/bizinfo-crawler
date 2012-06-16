e6420622dcc2acbccb5d180ae314e439e2498296
paranoiase Kang <paranoiase@gmail.com>
2012-06-16 10:49:05 +0900

    should add stub of RSS data

--- runner.rb
+++ runner.rb
@@ -2,9 +2,20 @@ require 'sinatra'
 require './lib/board'
 require './lib/crawler'
 
+class RSSInfo
+  class << self
+    def get(key)
+      case key
+      when 'venture'
+        'http://www.bizinfo.go.kr/userPolicyInfoList.do?policyInfoSerchTO.lclasId=5000'
+      end
+    end
+  end
+end
+
 get '/venture' do
   board = Board.new
   crawler = Crawler.new
-  crawler.crawl(board, 'http://www.bizinfo.go.kr/userPolicyInfoList.do?policyInfoSerchTO.lclasId=5000')
+  crawler.crawl(board, RSSInfo.get('venture'))
   board.to_rss
 end

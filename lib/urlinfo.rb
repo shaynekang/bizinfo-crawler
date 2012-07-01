af2c754f13e6bfbf790881495990ee8ba205d096
paranoiase Kang <paranoiase@gmail.com>
2012-07-01 17:39:09 +0900

    rename config/url.yaml to config/url.yml

--- lib/urlinfo.rb
+++ lib/urlinfo.rb
@@ -4,7 +4,7 @@ require 'yaml'
 class URLInfo
   class << self
     def get(key)
-      YAML.load_file("config/url.yaml")["bizinfo"][key]
+      YAML.load_file("config/url.yml")["bizinfo"][key]
     end
   end
 end
